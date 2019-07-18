ac648464df8b1f38247c21982a9b5fb44a63c1f2
Diablo9946 <diabloch9946@gmail.com>
2019-07-18 08:00:24 +0200

    Scheleton for the project.

---
+++ action.js
@@ -0,0 +1,17 @@
+// Faccio un controllo
+// console.log("Ciao");
+
+// Mi preparo le variabili
+
+// Creo l'array degli cognomi
+
+// Chiedo il cognome all'utente
+
+
+// Inserisco il cognome dell'utente nell'array
+
+
+// Stampo la lista nell'ordine alfabetico
+
+
+// Dico all'utente a che posto si trovi
