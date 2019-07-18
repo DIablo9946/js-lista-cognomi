00a78e686d51f0d03e5a4ba3b7afc97263c7c564
Diablo9946 <diabloch9946@gmail.com>
2019-07-18 08:09:43 +0200

    Printing emails.

--- action.js
+++ action.js
@@ -3,15 +3,25 @@
 
 // Mi preparo le variabili
 
+var cognomi, cognutente, stampa;
+
+stampa = document.getElementById('my');
 // Creo l'array degli cognomi
 
-// Chiedo il cognome all'utente
+cognomi = ["Moretti", "Alfani", "Bartolini", "Zazzone", "Privotti"];
+console.log(cognomi);
 
+
+// Chiedo il cognome all'utente
+cognutente = prompt("Inserisci il Tuo cognome");
+console.log("Il cognome dell'utente è " + cognutente);
 
 // Inserisco il cognome dell'utente nell'array
-
+cognomi.push(cognutente);
+console.log(cognomi);
 
 // Stampo la lista nell'ordine alfabetico
-
+stampa.innerHTML = cognomi;
 
 // Dico all'utente a che posto si trovi
+// stampa.innerHTML = "Il Tuo cognome si trova al " + " posto";
